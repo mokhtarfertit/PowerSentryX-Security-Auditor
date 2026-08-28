a59dad9ce3aef1468fbdee679001357c4ba90f86
mokhtar <mokhtarfertit@gmail.com>
2026-08-28 19:50:14 +0100

    make check administrator privileges

---
+++ utils/PrivilegeCheck.psm1
@@ -0,0 +1,22 @@
+function Test-PowerSentrXAdminstrator {
+    [CmdletBinding()]
+    param ()
+
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+
+    try {
+        $principal = [System.Security.Principal.WindowsPrincipal]::new(
+            $identity
+        )
+
+        return $principal.IsInRole(
+            [System.Security.Principal.WindowsBuiltInRole]::Administrator
+        )
+    }
+
+    finally {
+        $identity.Dispose()
+    }
+}
+
+Export-ModuleMember -Function Test-PowerSentryXAdminstrator
